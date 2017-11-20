1da5d90820788fa3f4b63d149365740d9cde43c1
Abhishek Bisht <abhi.bisht89@gmail.com>
2017-11-19 20:58:37 -0800

    Update utils.py
    make some changes now handling exception from wiki module so that user know how to make specific search.

--- utils.py
+++ utils.py
@@ -1,29 +1,35 @@
 from wit import Wit 
 import wikipedia as wk
 
-access_token = "" #paste here your wit.ai app Server Access Token
+access_token = "Y5FPTWSLB5CGOOCCES4L246P2GM6FRHO"
 
 client = Wit(access_token = access_token)
 
+
 def wit_response(message_text):
 	resp = client.message(message_text)
-	print("response is:"+str(resp))
+	#print("response is:"+str(resp))
 	if not resp['entities']:
 		return "Please ask something like:\nTell me something about "+message_text+"\nWho is "+message_text+"\nWhat is "+message_text
 	else:	
-		print("next response is:"+str(resp['entities']['wikipedia_search_query'][0]['value']))
+		#print("next response is:"+str(resp['entities']['wikipedia_search_query'][0]['value']))
 		return (resp['entities']['wikipedia_search_query'][0]['value'])
 
 
 def get_wiki_content(categories):
-
 	try:
+		listoption=''
 		title=wk.search(categories)[0]
 		page=wk.page(title)
+		urlinfo=page.url
 		content=wk.summary(categories).splitlines()
-		print(content)
-		return content
-
-	except Exception as ex:	
-		print ("util class exp"+str(ex))
-		return "I have more that one reuslt for your search query,Please be specific."
+		#print(content,urlinfo)
+		return content ,urlinfo
+	except:	
+		topics = wk.search(categories)
+		initial='Please be specific '+categories+" may refer to: "
+		for i, topic in enumerate(topics):
+			if i==0:
+			   continue
+			listoption=listoption+str(i)+' '+ topic+' '+'\n'
+	return initial +'\n'+listoption,'Search from above recommendation.'
